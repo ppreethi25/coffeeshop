633a54f1d00c68dd6121eb1c78aedb3722cf43bf
ppreethi25 <72737532+ppreethi25@users.noreply.github.com>
2021-08-18 12:56:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function() {
+
+    jQuery('.autoplay').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        next: false,
+        prev: false,
+        dots: false,
+      });
+    });
+  //   jQuery(document).ready(function() {
+  //   jQuery('.fade').slick({
+  //     dots: true,
+  //     infinite: true,
+  //     speed: 500,
+  //     fade: true,
+  //     cssEase: 'linear'
+  //   });
+  // });
